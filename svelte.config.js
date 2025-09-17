81307a537d020df0978fb836eb27b1b8f293049d
jaspwr <40232406+jaspwr@users.noreply.github.com>
2025-09-17 23:03:57 +1000

    fix: workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	// preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
+			out: "build",
 			pages: 'build',
 			assets: 'build',
 			fallback: '404.html',
