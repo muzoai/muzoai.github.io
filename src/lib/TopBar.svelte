4e0f232b04e719e8400db27899531b6f5dd64285
jaspwr <40232406+jaspwr@users.noreply.github.com>
2025-10-01 22:38:49 +1000

    connect to backend

---
+++ src/lib/TopBar.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { elemHoverSound } from "$lib";
+	import { IconSettings } from "@tabler/icons-svelte";
+	import { getShowingSettings, setShowingSettings } from "./settings.svelte";
+
+</script>
+
+<div class="container">
+	<img class="logo" src="logo-word-blue.png" alt="logo" />
+
+	<button
+		aria-label="settings"
+		use:elemHoverSound
+		onclick={() => setShowingSettings(!getShowingSettings())}
+	>
+		<IconSettings size={32} />
+	</button>
+</div>
+<div class="spacer"></div>
+
+<style>
+	:root {
+		--top-bar-height: 50px;
+	}
+
+	.spacer {
+		height: var(--top-bar-height);
+	}
+
+	.container {
+		height: var(--top-bar-height);
+		top: 0px;
+		right: 0px;
+		position: fixed;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		padding: 10px;
+		margin: 0px;
+		background-color: var(--bg-sec);
+	}
+
+	.logo {
+		height: 50px;
+		margin-left: 30px;
+	}
+</style>
